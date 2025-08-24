a53fa4352f656b623c2614db2138f258e470fb03
yoonseo23 <jysjdt03gmail.com>
2025-08-25 00:58:09 +0900

    [CONFIG] vite 구성 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/homework_react/',
+  build: {
+    outDir: 'docs',
+  },
 })
