2a1b5a2f83a927298191f533478b01a8671d742b
yoonseo23 <jysjdt03gmail.com>
2025-08-31 02:10:05 +0900

    [INIT] App에 3주차 과제 삽입

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,10 @@
-import Form from "./components_w2/form";
+import MainPage from "./components_w3/main-page";
 import "./styles/app.css";
 
 export function App() {
   return (
     <div className="app">
-
-
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
-      <h2>2주차 과제 - 회원가입/로그인 컴포넌트 제작</h2>
-      <Form />
+      <MainPage />
     </div>
   );
 }
